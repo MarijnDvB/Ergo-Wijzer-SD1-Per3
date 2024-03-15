8d92b288bb341d95a8aa8f7c0753ce7564b6b18c
589180ROC <589180@edu.rocmn.nl>
2024-03-15 12:41:52 +0100

    Slider almost done
    Buttons need arrows and ergonomic bottle needs to be added still.

--- product-tiles.js
+++ product-tiles.js
@@ -1,28 +1,41 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// I will be creating a different pen with touch support but right // now I have no time for it due to school
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+const slider = document.querySelector(".items");
+const slides = document.querySelectorAll(".item");
+const button = document.querySelectorAll(".button");
+
+let current = 0;
+let prev = 4;
+let next = 1;
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+
+const gotoNum = number => {
+    current = number;
+    prev = current - 1;
+    next = current + 1;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+        slides[i].classList.remove("prev");
+        slides[i].classList.remove("next");
+    }
+
+    if (next == 5) {
+        next = 0;
+    }
+
+    if (prev == -1) {
+        prev = 4;
+    }
+
+    slides[current].classList.add("active");
+    slides[prev].classList.add("prev");
+    slides[next].classList.add("next");
 }
